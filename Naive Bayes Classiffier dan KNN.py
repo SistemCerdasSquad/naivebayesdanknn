22f491891778e13b070a7606d99f1fff8950fc79
restureese <restureese@gmail.com>
2018-05-09 22:26:57 +0700

    update

--- Naive Bayes Classiffier dan KNN.py
+++ Naive Bayes Classiffier dan KNN.py
@@ -1,18 +1,9 @@
-
-# coding: utf-8
-
 # # Load Data Set
-
-# In[ ]:
-
 import pandas as pd
 df = pd.read_csv('dataset.csv')
 
 
 # # Save DataSet into Data Structure
-
-# In[ ]:
-
 X = []
 y = []
 for (name,mcg,gvh,lip,chg,acc,alm1,alm2,label) in zip(df['sequence_name'],df['mcg'],df['gvh'],df['lip'],df['chg'],df['aac'],df['alm1'],df['alm2'],df['label']):
@@ -21,17 +12,11 @@ for (name,mcg,gvh,lip,chg,acc,alm1,alm2,label) in zip(df['sequence_name'],df['mc
 
 
 # # Membagi Data Testing dan Data Training
-
-# In[ ]:
-
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
 
 # # Training Model
-
-# In[ ]:
-
 from sklearn.naive_bayes import GaussianNB
 from sklearn.neighbors import KNeighborsClassifier
 
@@ -43,15 +28,9 @@ clf.fit(X_train, y_train)
 
 
 # # Testing Model
-
-# In[ ]:
-
 for test in X_test:
     result = cls.predict([test])
     print("nilai {nilai} memiliki label {label}".format(nilai=test,label=result[0]))
-
-
-# In[ ]:
 
 for test in X_test:
     result = clf.predict([test])
@@ -59,13 +38,6 @@ for test in X_test:
 
 
 # # Akurasi
-
-# In[ ]:
-
 print("Akurasi Prediksi : ",cls.score(X_test,y_test))
 
-
-# In[ ]:
-
 print("Akurasi Prediksi : ",clf.score(X_test,y_test))
-
